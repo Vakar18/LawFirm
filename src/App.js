c234c14454580ff86fe8bdab4eeadd77eb9d3ec5
vakar <vakarahmad99@gmail.com>
2023-11-30 14:03:55 +0530

    Adding Navbar and Hero Section

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import Navbar from "./Components/Navbar/navbar"
+import HeroSection from './Components/HeroSection/heroSection'
+// import AboutSection from './components/AboutSection/AboutSection'
+// import LawSection from './components/LawSection/LawSection'
+// import TestimonialSection from './components/TestimonialSection/TestimonialSection'
+// import OurTeamSection from './components/OurTeamSection/OurTeamSection'
+// import FaqSection from './components/FaqSection/FaqSection'
+// import Newsletter from './components/Newsletter/Newsletter'
+// import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      {/* <AboutSection /> */}
+      {/* <LawSection />
+      <TestimonialSection />
+      <OurTeamSection />
+      <FaqSection />
+      <Newsletter />
+      <Footer /> */}
+    </>
+  )
+}
+
+export default App
